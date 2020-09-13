e73976c4dd5985edf7d4c12c3de64eb67cce72ff
Zach Johnson <zlg23johnson@gmail.com>
2020-09-13 10:22:49 -0500

    Working changing opacity

--- pages/upload.js
+++ pages/upload.js
@@ -8,15 +8,15 @@ export default function Upload() {
   const [workingImage, setWorkingImage] = useState('');
   const [imageTitle, setImageTitle] = useState('');
   const [error, setError] = useState('');
+  const [opacity, setOpacity] = useState(1.0);
 
   const onFormSubmit = (e) => {
     e.preventDefault();
     if (!file) {
-      setError('Please Upload a File');
+      setError('Please Upload an Image File');
       return;
     }
     fileUpload(file).then((response) => {
-      console.log('In response', response.data);
       setWorkingImage(response.data.path);
       setImageTitle(response.data.title);
       setFile();
@@ -33,10 +33,9 @@ export default function Upload() {
 
   const isValidImage = (file) => {
     const allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
-    console.log(typeof file.name);
-    debugger;
     if (!allowedExtensions.exec(file.name)) {
       setError('Please upload an jpg/jpeg, png, or gif image');
+      setFile();
       return false;
     }
 
@@ -56,18 +55,44 @@ export default function Upload() {
     return post(url, formData, config);
   };
 
+  const handleUpdate = (e) => {
+    // handlue ui update
+    setOpacity(e.target.value);
+    // update the variable
+    document.documentElement.style.setProperty('--opacity', e.target.value);
+  };
+
   return (
     <>
       <form className={styles.form} onSubmit={onFormSubmit}>
         <h1>Upload an image and change it's opacity</h1>
-        <input type="file" onChange={onFormChange} />
+        <input type="file" name="file" id="file" className={styles.upload} onChange={onFormChange} />
+        <label htmlFor="file" className={styles.uploadLabel}>
+          Choose a file
+        </label>
+        {file && <span className={styles.fileToUpload}>Your Chosen File: {file.name}</span>}
         {error !== '' && <span className={styles.error}>{error}</span>}
-        <button type="submit">Upload</button>
+        <button type="submit" className={styles.submit}>
+          Upload
+        </button>
       </form>
 
       {workingImage !== '' && (
         <section className={styles.imageContainer}>
           <h2 className={styles.title}>Uploaded Image: {imageTitle}</h2>
+          <section className={styles.control}>
+            <label htmlFor="opacity">Change Opacity: </label>
+            <input
+              onChange={(e) => handleUpdate(e)}
+              id="opacity"
+              type="range"
+              name="opacity"
+              min="0.0"
+              max="1.0"
+              step="0.05"
+              value={opacity}
+            />
+          </section>
           <figure className={styles.figure}>
             <img className={styles.image} src={workingImage} />
           </figure>
